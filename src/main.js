e8105ec3320496fe7b8ed746af3d42dea4bd6074
PeratX <1215714524@qq.com>
2019-11-10 11:43:30 +0800

    app: use vuetify

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
