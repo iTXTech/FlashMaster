0b369c85dd4f4dd9f6480ae0f4172496454d3e55
PeratX <1215714524@qq.com>
2019-11-10 14:35:30 +0800

    app: add drawer

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
-# flashmaster
+# iTXTech FlashMaster
 
-## Project setup
-```
+Powerful NAND Flash Part Number and Id querying toolbox.
+
+## Introduction
+
+The re-implementation of FlashMaster in Vue.js and Vuetify.
+
+Backend: [iTXTech FlashDetector](https://github.com/iTXTech/FlashDetector)
+
+## Setup
+
+```bash
+git clone https://github.com/iTXTech/FlashMaster
+cd FlashMaster
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
 npm run serve
 ```
+## License
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+    Copyright (C) 2019 iTX Technologies
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,92 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row v-if="item.heading" :key="item.heading" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon
+          >
+            <template v-slot:activator>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.text }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            <v-list-item v-for="(child, i) in item.children" :key="i" link>
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>{{ child.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else :key="item.text" link>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-img
+        alt="FlashMaster Logo"
+        :src="require('./assets/logo.png')"
+        class="shrink mr-2"
+        contain
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="hidden-sm-and-down">iTXTech FlashMaster</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn href="https://github.com/iTXTech/FlashMaster" target="_blank" text>
+        <span class="mr-2">GitHub</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-content></v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+  props: {
+    source: String
+  },
+  name: "FlashMaster",
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
-  }),
+    dialog: false,
+    drawer: null,
+    items: [
+      { text: "料号查询" },
+      { text: "料号搜索" },
+      { text: "闪存ID查询" },
+      { text: "设置" },
+      { text: "关于" }
+    ]
+  })
 };
 </script>
