6895e58fb879444ee42866e584dfeeaf82902034
PeratX <1215714524@qq.com>
2019-11-11 18:41:56 +0800

    app: change to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: "/",
-    component: () => import("@/views/Decode")
+    redirect: "/decode"
   },
   {
     path: "/decode",
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   scrollBehavior: () => ({ y: 0 }),
   routes
